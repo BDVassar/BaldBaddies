414931b5f18857d0509ee214ee5dad113dc46896
Derik Banning <deriklee451@gmail.com>
2022-12-02 14:36:15 -0700

    modal form

--- client/app/Models/Posts.js
+++ client/app/Models/Posts.js
@@ -25,4 +25,34 @@ export class Post {
     `
   }
 
+
+
+
+  static postForm() {
+    return `
+    <div class="modal-header">
+    <h1 class="modal-title fs-5" id="exampleModalLabel">Post that Shine</h1>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+  <div class="modal-body">
+    <form onsubmit="app.postsController.createPost()">
+      <div class="mb-3">
+        <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+      </div>
+      <div class="mb-3">
+        <input type="url" class="form-control" id="imgUrl" name="imgUrl" placeholder="That Bald head Url">
+      </div>
+      <div class="mb-3">
+        <input type="text" class="form-control" id="des" name="des" placeholder="Tell us about that pic">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    <button type="button" class="btn btn-primary">Save changes</button>
+  </div>
+    `
+  }
+
 }
