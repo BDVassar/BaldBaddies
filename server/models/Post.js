d9aaaf502a4f7bfb4566d4c0beb274838e1dbfd6
Bryan Vassar <vassar.bryan@gmail.com>
2022-12-02 11:57:12 -0700

    max length added to post url, and virtual for account info added

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,11 +1,17 @@
 import { Schema } from "mongoose";
 
 export const PostSchema = new Schema(
-    {
-        title: { type: String, required: true },
-        imgUrl: { type: String, required: true},
-        des: { type: String, required: true },
-        creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
-    },
-    { timestamps: true, toJSON: { virtuals: true } }
-  )
+  {
+    title: { type: String, required: true },
+    imgUrl: { type: String, required: true, maxLength: 400 },
+    des: { type: String, required: true },
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+)
+
+PostSchema.virtual('Account', {
+  localField: 'creatorId',
+  ref: 'Account',
+  foreignField: '_id'
+})
--- server/services/PostService.js
+++ server/services/PostService.js
@@ -2,7 +2,7 @@ import { dbContext } from "../db/DbContext.js"
 
 class PostService {
   async getAllPost() {
-    const posts = await dbContext.Post.find()
+    const posts = await dbContext.Post.find().populate('Account')
     return posts
   }
   async createPost(body) {
