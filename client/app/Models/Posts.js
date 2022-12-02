b0131c805cbb330f7d54538dbe136e044ff66d0a
calvin thurston <calvin.thurston@gmail.com>
2022-12-02 11:30:02 -0700

    push

--- client/app/Models/Posts.js
+++ client/app/Models/Posts.js
@@ -13,7 +13,7 @@ export class Post {
     return `
     <div class="col-4 p-4">   
         <div class="elevation-3">
-          <img class="post-card" src="https://thiscatdoesnotexist.com" alt="post-image">
+          <img class="post-card" src="${this.imgUrl}" alt="post-image">
           <div class="d-flex justify-content-between">
             <i class="mdi mdi-chat fs-1 selectable" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
             <i class="mdi mdi-thumb-up fs-1 selectable"></i>
